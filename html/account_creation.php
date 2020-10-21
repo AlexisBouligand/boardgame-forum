efeb712165a6eb5973eefc3ae65c05cf2b461609
kalharko <oscar.dewasmes@gmail.com>
2020-10-21 18:20:51 +0200

    account creation

--- html/account_creation.php
+++ html/account_creation.php
@@ -5,40 +5,52 @@ include_once("../lib/head.php");
 
 <h2>Account creation</h2>
 
-<form class="main-form" method="post" action="fictif.php">
-  <div class="text-input">
-    <label for="pseudo">Pseudo:</label>
-    <input type="text" name="pseudo" id="pseudo" required />
-  </div>
+<form class="main-form" method="post" action="../lib/add_user.php">
 
-  <div class="text-input">
-    <label for="email">Email:</label>
-    <input type="text" name="email" id="email" required />
-  </div>
+    <label>
+        Pseudo:<input type="text" name="pseudo" required />
+    </label>
 
-  <div class="text-input">
-    <label for="password">Password:</label>
-    <input type="password" name="password" id="password" required />
-  </div>
 
-  <div class="text-input">
-    <label for="Birthdate">Birthdate:</label>
-    <input type="text" name="Birthdate" id="Birthdate" required />
-  </div>
+    <label>
+        Email:<input type="email" name="email" required />
+    </label>
 
-  <div class="dropdown-menu">
-    Country:
-    <select name="country" id="country-select">
-      <option name="AL">Albanie</option>
-      <option name="FR" selected>France</option>
-      <option name="WK">Wakanda</option>
+
+    <label>
+        Password:<input type="password" name="password" required />
+    </label>
+
+
+    <label>
+        Birthdate:<input type="date" name="birthdate" required />
+    </label>
+
+
+    <label>Country:</label>
+    <select name="country">
+        <?php
+        // Se connecter à la base de données
+        $bdd = new PDO("mysql:host=localhost;dbname=boardgameforum;charset=utf8", "root", "");
+        // Créer une requête SELECT pour récupérer
+        $req = $bdd->prepare("SELECT id, country_name FROM country;");
+        // Exécute la requête
+        $req->execute();
+        // Tant que il y a des données ->
+        $ligne = $req->fetch();
+        while($ligne) {
+            echo "<option value=\"$ligne[id]\">$ligne[country_name]</option>";
+            $ligne = $req->fetch();                     // Passer à la ligne suivante
+        }
+        ?>
     </select>
-  </div>
 
-  <!-- TODO: this -->
-  <!-- <input type="submit" name="pp_button" value="chose profile picture"> -->
 
-  <input type="submit" name="send_button" value="Send">
+    <label>
+        profile picture:<input type="file" name="profile_picture"/>
+    </label>
+
+    <input type="submit" name="send_button" value="Send"/>
 </form>
 
 <?php
