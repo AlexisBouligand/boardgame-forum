9d44a27e7e797b3eb7510e7037248293a58f4a91
Adrien Burgun <adrien.burgun@orange.fr>
2020-11-04 12:39:58 +0100

    :sparkles: Beautiful game page and karma buttons

--- pages/game_page.php
+++ pages/game_page.php
@@ -1,6 +1,6 @@
 <?php
 $PAGE_NAME = "Game Page";
-$PAGE_HEAD = "<script src=\"/js/vote.js\" defer></script><link rel=\"stylesheet\" href=\"/css/game_page.css\" />";
+$PAGE_HEAD = "<script src=\"/js/game_page.js\" defer></script><link rel=\"stylesheet\" href=\"/css/game_page.css\" />";
 include_once("../lib/head.php");
 include_once("../lib/add_review.php");
 
@@ -19,13 +19,13 @@ if (!$game_res) {
 //We'll make a loop to get all the critics
 //We first need to het the number of critics
 $count_req = $bdd->prepare("SELECT COUNT(*) AS counter FROM review WHERE id_game = ?");
-if ($count_req->execute([$game_res->id])){
+if ($count_req->execute([$game_res->id])) {
     $count_res = $count_req->fetch();
 }
 
 //We get the critics
 $review_req = $bdd->prepare("SELECT * FROM review WHERE id_game = ?");
-if($review_req->execute([$game_res->id])){
+if($review_req->execute([$game_res->id])) {
     $access_to_critic=true;
     $review_res = $review_req->fetch();
 }
@@ -33,40 +33,52 @@ if($review_req->execute([$game_res->id])){
 
 $search_res = NULL;
 //We put the critics inside a Critic object
-for($i = 0; $i < $count_res['counter']; $i++){
-    if($access_to_critic){//If everything went went during the query
+for($i = 0; $i < $count_res["counter"]; $i++) {
+    if($access_to_critic) { //If everything went went during the query
 
         //We search for the username of the user who write the critic
         $user_req = $bdd->prepare("SELECT * FROM user WHERE id=?");
 
-        if($user_req->execute([$review_res['id_user']])) {
-            $user_res = $user_req->fetch();//We save the user to use him/her then
+        if($user_req->execute([$review_res["id_user"]])) {
+            $user_res = $user_req->fetch(); //We save the user to use him/her then
 
             //Get the karma of the current review
             $karma_req = $bdd->prepare("SELECT SUM(positive) FROM vote WHERE id_review = ?;");
-            $karma_req->execute([$review_res['id']]);
-            $karma_res = $karma_req->fetch();
-            if ($karma_res[0] == NULL){
-                $karma_res[0] = 0;
+            if ($karma_req->execute([$review_res["id"]])) {
+                $karma_res = $karma_req->fetch();
+                if ($karma_res[0] == NULL) {
+                    $karma_res[0] = 0;
+                }
+            }
+
+            $current_vote = 0;
+            if ($current_user) {
+                $current_vote_req = $bdd->prepare("SELECT * FROM vote WHERE id_user = ? AND id_review = ?;");
+                if (
+                    $current_vote_req->execute([$current_user->id, $review_res["id"]])
+                    && ($current_vote_res = $current_vote_req->fetch())
+                ) {
+                    $current_vote = $current_vote_res["positive"];
+                }
             }
 
 
             $search_res[$i] = new Critic(
-                $review_res['id'],
-                new Player($user_res['id'], $user_res['pseudonym'], time(), $user_res['country'], $user_res['password'], $user_res['profile_picture']),
-                $review_res['comment'],
-                $review_res['date_publication'],
+                $review_res["id"],
+                new Player($user_res["id"], $user_res["pseudonym"], time(), $user_res["country"], $user_res["password"], $user_res["profile_picture"]),
+                $review_res["comment"],
+                $review_res["date_publication"],
                 $karma_res[0],
-                $review_res['score']
+                $review_res["score"],
+                $current_vote
             );
-//        echo $search_res[$i]->id;
             $review_res = $review_req->fetch();
         }
     }
 }
 
 //If there is no comments published for this game, we display this comment
-if ($search_res == NULL){
+if ($search_res == NULL) {
     $search_res = [new Critic(
         0,
         new Player(0, "[phantom]", 0, "FR", "..."),
@@ -79,7 +91,7 @@ if ($search_res == NULL){
 //Ask for a game and a user
 function try_create_review($game_res, $current_user) {
 
-    if ($current_user == NULL){
+    if ($current_user == NULL) {
         return "You need to be connected to submit a review";
     }
 
@@ -89,6 +101,8 @@ function try_create_review($game_res, $current_user) {
     $id_game = $game_res->id;
     $id_user = $current_user->id;
 
+    if (!is_numeric($score) || $score < 0 || $score > 10 || $score != round($score)) return "Invalid parameter: score";
+
     $res = add_review(
         $score,
         $comment,
@@ -96,7 +110,7 @@ function try_create_review($game_res, $current_user) {
         $id_user
     );
     if ($res == NULL) {
-        header("Refresh:0");
+        header("Refresh: 0");
     } else {
         return "There was an error while trying to add your review: " . $res;
     }
@@ -132,24 +146,33 @@ if (isset($_POST["submit"])) {
     </aside>
   </article>
 
-    <?php
-    //If the user is connected, he can se the button "Write Review"
-    if($current_user){
-        ?>
+  <?php
+  //If the user is connected, he can see the button "Edit game" and the button "Write Review"
+  if ($current_user) {
+      echo "<div class=\"game-edition-buttons\">";
+      echo "<button class=\"open-button\" onclick=\"toggle_form()\">Write Review</button>";
+      echo "<a href=\"/game_edition.php?id=" . $game_res->id . "\" class=\"center gray\">Edit game</a>";
+      echo "</div>";
+  } ?>
 
-        <button class="open-button" onclick="openForm()">Write Review</button>
-        <?php
-    }
-    ?>
+  <!-- Write Review Form -->
 
-    <?php
-    //If the user is connected, he can se the button "Edit game"
-    if ($current_user) {
-        echo "<div class=\"vertical-spacer\"></div>";
-        echo "<a href=\"/game_edition.php?id=" . $game_res->id . "\" class=\"center gray\">Edit game</a>";
-    }
+  <div class="form-popup" id="form-popup">
+      <form method="post" action="game_page.php?id=<?php echo $game_res->id; ?>" class="form-container">
+          <h2>Write review</h2>
 
+          <label>
+              <b>Score : </b>
+              <input type="number" placeholder="Score" name="score" min="0" max="10" required>
+          </label>
+          <label for="comment"><b>Comment:</b></label>
+          <input type="text" placeholder="Enter your comment (not required)" name="comment">
+          <button type="submit" name="submit" class="btn">Send</button>
+          <a class="btn cancel" onclick="close_form()">Cancel</a>
+      </form>
+  </div>
 
+  <?php
     //Display the critics while they aren't all displayed
     foreach ($search_res as $critic) {
         ?>
@@ -175,11 +198,31 @@ if (isset($_POST["submit"])) {
             <div><?php echo $critic->score; ?>/10</div>
 
             <div class="karma-box">
-                <a class="like" name="like">
+                <a
+                  name="like"
+                  <?php if ($critic->current_vote == 1) {
+                    echo "class=\"like active\"";
+                  } else {
+                    echo "class=\"like\"";
+                  } ?>
+                  <?php if ($current_user) { ?>
+                    onclick="vote(<?php echo $critic->id; ?>, 1, this);"
+                  <?php } ?>
+                >
                     <span>&#x25b2;</span>
                 </a>
                 <span class="karma"><?php echo $critic->karma; ?></span>
-                <a class="dislike" name="dislike">
+                <a
+                  name="dislike"
+                  <?php if ($critic->current_vote == -1) {
+                    echo "class=\"dislike active\"";
+                  } else {
+                    echo "class=\"dislike\"";
+                  } ?>
+                  <?php if ($current_user) { ?>
+                    onclick="vote(<?php echo $critic->id; ?>, -1, this);"
+                  <?php } ?>
+                >
                     <span>&#x25bc;</span>
                 </a>
             </div >
@@ -190,38 +233,6 @@ if (isset($_POST["submit"])) {
     ?>
 
 </section>
-
-
-
-<!-- Write Review Form -->
-
-<div class="form-popup" id="myForm">
-    <form method="post" action="game_page.php?id=<?php echo $game_res->id; ?>" class="form-container">
-        <h2>Write review</h2>
-
-        <label for="score"><b>Score : </b></label>
-        <input type="number" placeholder="Score" name="score" min="0" max="10" required>
-        <br>
-        <label for="comment"><b>Comment:</b></label>
-        <input type="text" placeholder="Enter your comment (not required)" name="comment">
-        <br><br>
-        <button type="submit" name="submit" class="btn">Send</button>
-        <button type="submit" class="btn cancel" onclick="closeForm()">Cancel</button>
-    </form>
-</div>
-
-
-<script>
-    function openForm() {
-        document.getElementById("myForm").style.display = "block";
-    }
-
-    function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-    }
-</script>
-
-
 
 <?php
 include_once("../lib/tail.php");
